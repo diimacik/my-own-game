369f5947727e47e11bf23e04bbd8306fc6bd269e
Dmytro <gagandima534@gmail.com>
2024-07-17 20:42:25 +0200

    added text for gameOver

--- UI.js
+++ UI.js
@@ -21,6 +21,28 @@ export class UI {
         for (let i = 0; i < this.game.energy; i++) {
             context.drawImage(this.energyImage, 30 * i + 20, 100, 25, 25);
         }
+        if (this.game.gameOver) {
+            let text = 'Game Over, press \n Enter or swipe down \n to restart this game';
+            let x = this.game.width / 2;
+            let y = 150;
+            let linehieght = 40;
+            let lines = text.split('\n');
+
+            for (let i = 0; i < lines.length; i++) {
+                context.textAlign = 'center';
+        
+                context.fillStyle = 'rgb(0, 0, 0)';
+                context.fontSize = 50;
+                context.fillText(lines[i], x, y + (i * linehieght));
+                context.fillStyle = 'rgb(255, 255, 255)';
+                context.fontSize = 50;
+                context.fillText(lines[i], x + 3, y + 3 + (i * linehieght));
+            }
+            
+            
+
+            //context.fillText(text, x, y);
+        }
         context.restore();
 
     }
